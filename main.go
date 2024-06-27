7a51e2c9825f992149fd8f5866b18b658ff09bb3
Taras Shcherban <147646967+Miki535@users.noreply.github.com>
2024-06-27 12:46:19 +0300

    update

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type Currency struct {
+	Ccy     string `json:"ccy"`
+	BaseCcy string `json:"base_ccy"`
+	Buy     string `json:"buy"`
+	Sale    string `json:"sale"`
+}
+
+func main() {
+	parse("https://api.privatbank.ua/p24api/pubinfo?exchange&coursid=5")
+}
+
+func parse(url string) {
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Println(err)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	_ = resp.Body.Close()
+	var currencies []Currency
+	err = json.Unmarshal(body, &currencies)
+	if err != nil {
+		log.Println(err)
+	}
+	for _, currency := range currencies {
+		a := currency.Ccy
+		b := currency.BaseCcy
+		c := currency.Buy
+		d := currency.Sale
+		fmt.Println(a, b, c, d)
+	}
+}
